90150377fd0d46b0f619c7bea15bffd7b04415ee
Meraj Abbas <meraj.ma5@gmail.com>
2024-04-09 02:36:47 +0530

    completed update functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from "../firebase";
-import { signOutUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart } from "../redux/user/userSlice";
+import { signOutUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart, updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
 
 
 
@@ -11,12 +11,15 @@ import { signOutUserFailure, deleteUserFailure, deleteUserStart, deleteUserSucce
 
 export const Profile = () => {
     const fileRef = useRef(null);
-    const {currentUser} = useSelector((state)=>state.user);
+    const {currentUser,loading,error} = useSelector((state)=>state.user);
     const [file,setFile] = useState(undefined);
     const [filePerc ,setFilePerc] = useState(0); 
     const [fileUploadError,setFileUploadError] = useState(false);
     const [formData,setFormData] = useState({});
-    const dispatch = useDispatch()
+    const [updateSuccess,setUpdateSuccess] = useState(false);
+    const dispatch = useDispatch();
+
+    console.log(formData);
     
 
 
@@ -59,6 +62,7 @@ export const Profile = () => {
     const handleDeleteUser = async()=>{
         try {
             dispatch(deleteUserStart());
+            
             const res = await fetch(`api/user/delete/${currentUser._id}`,{
                 method: "DELETE",
             });
@@ -91,10 +95,39 @@ export const Profile = () => {
             
         }
     }
+    const handleChange = (e)=>{
+        setFormData({...formData,[e.target.id]: e.target.value});
+
+    }
+    const handleSubmit= async(e)=>{
+        e.preventDefault();
+        try {
+            dispatch(updateUserStart());
+
+            const res = await fetch(`api/user/update/${currentUser._id}`,{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body : JSON.stringify(formData),
+
+            });
+            const data = await res.json();
+            if(data.success === false){
+                dispatch(updateUserFailure(data.message))
+                return;
+            }
+            dispatch(updateUserSuccess(data));
+            setUpdateSuccess(true);
+            
+        } catch (error) {
+            dispatch(updateUserFailure(error.message));
+        }
+    }
 
     return <div className="p-3 max-w-lg mx-auto"> 
         <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept="image/*" />
             <img src={formData.avatar ||currentUser.avatar} alt="profile" onClick={()=>{fileRef.current.click()}} className="rounded-full h-24 w-24 self-center cursor-pointer object-cover" />
             <p className=" text-sm self-center">
@@ -103,14 +136,16 @@ export const Profile = () => {
             filePerc==100 ? (<span className="text-green-700">Image Sucessfully uploaded!!</span>) : ""
          }
          </p>
-            <input type="text" id="username" placeholder="username" className="border p-3  rounded-lg"/>
-            <input type="email" id="email" placeholder="email" className="border p-3  rounded-lg"/>
-            <input type="text" id="password" placeholder="password" className="border rounded-lg p-3 "/>
-            <button className="bg-slate-700 p-3 border rounded-lg text-white uppercase hover:opacity-80">Update</button>
+            <input onChange={handleChange} defaultValue={currentUser.username} type="text" id="username" placeholder="username" className="border p-3  rounded-lg"/>
+            <input onChange={handleChange} defaultValue={currentUser.email} type="email" id="email" placeholder="email" className="border p-3  rounded-lg"/>
+            <input onChange={handleChange} type="password" id="password" placeholder="password" className="border rounded-lg p-3 "/>
+            <button disabled = {loading} className="bg-slate-700 p-3 border rounded-lg text-white uppercase hover:opacity-80" > { loading ? 'Loading...':'Update'}</button>
         </form>
         <div className="flex justify-between mt-5">
             <span onClick={ handleDeleteUser } className="text-red-700 cursor-pointer">Delete account</span>
             <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign out</span>
         </div>
+        <p className="text-red-700 mt-4">{error ? error: ''}</p>
+        <p className="text-green-700 mt-4">{updateSuccess ? 'User is updated sucessfully!':''}</p>
         </div>
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -53,8 +53,23 @@ const userSlice = createSlice({
             state.loading= false;
 
         },
+        
+        updateUserStart:(state)=>{
+            state.loading = true;  
+        },
+        updateUserSuccess:(state,action)=>{
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        updateUserFailure:(state,action)=>{
+            state.error = action.payload;
+            state.loading = false;
+            
+        },
+        
     }
 })
 
-export const {signInStart,signInSuccess,signInFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure,signOutUserStart,signOutUserSuccess,signOutUserFailure} = userSlice.actions;
+export const {signInStart,signInSuccess,signInFailure,deleteUserStart,deleteUserSuccess,deleteUserFailure,signOutUserStart,signOutUserSuccess,signOutUserFailure,updateUserFailure,updateUserStart,updateUserSuccess} = userSlice.actions;
 export default userSlice.reducer;
