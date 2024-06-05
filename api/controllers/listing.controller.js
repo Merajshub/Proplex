3dd1ab0802b7ddeca02cf59c8391e0f905f3985c
Meraj Abbas <meraj.ma5@gmail.com>
2024-06-05 16:53:39 +0530

    completed search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -67,3 +67,50 @@ export const getListing = async(req,res,next)=>{
     }
 
 }
+
+// for all 
+export const getListings = async(req,res,next)=>{
+    try {
+        const limit  = parseInt(req.query.limit)||'0';
+        const startIndex = parseInt(req.query.startIndex)||0;
+
+        let offer = req.query.offer;
+        if(offer === undefined || offer === 'false'){
+            offer = {$in : [false,true]};
+        }
+
+        let furnished = req.query.furnished;
+        if(furnished === undefined || furnished ==='false'){
+            furnished = {$in : [false,true]};
+        }
+
+        let parking= req.query.parking;
+        if(parking === undefined || parking ==='false'){
+            parking = {$in: [false,true]};
+        }
+        let type = req.query.parking;
+        if(type === undefined ||type==='all'){
+            type = {$in:['sale','rent']};
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name:{$regex: searchTerm,$options:'i'},
+            offer,
+            furnished,
+            parking,
+            type,
+        }).sort({[sort]:order}).limit(limit).skip(startIndex)
+        // For example, let's say you have a collection of documents with IDs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. If you set startIndex to 0 and limit to 5, MongoDB will retrieve documents with IDs 1, 2, 3, 4, and 5. If you set startIndex to 5 and limit to 5, MongoDB will skip the first 5 documents and retrieve documents with IDs 6, 7, 8, 9, and 10.
+
+        return res.status(200).json(listings);
+        
+    } catch (error) {
+        next(error);
+        
+    }
+
+}
